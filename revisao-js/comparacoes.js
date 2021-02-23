6d7622867481da81a371e14f3bbd04ac7cd9a263
hugomacielads <hugomacielcesar@gmail.com>
2021-02-23 01:11:26 +0000

    (22/02) Término da revisão JS

---
+++ revisao-js/comparacoes.js
@@ -0,0 +1,40 @@
+// Números
+let a = 10, b = 8, c = 10
+console.log(a > b)
+console.log(a < b)
+console.log(a == c)
+console.log(a != c)
+
+console.log('--------------------------')
+
+// Strings
+a = 'CATIFUNDA'
+b = 'FELISBINO'
+c = 'CATIFUNDA'
+
+console.log(a > b)
+console.log(a < b)
+console.log(a == c)
+console.log(a != c)
+
+console.log('--------------------------')
+
+// Booleanos
+a = true
+b = false
+c = true
+console.log(a > b)
+console.log(a < b)
+console.log(a == c)
+console.log(a != c)
+
+console.log('--------------------------')
+
+// Vetores
+a = [10, 20, 30, 40, 50]
+b = [-2, -3, -1]
+c = [10, 20, 30, 40, 50]
+console.log(a > b)
+console.log(a < b)
+console.log(a == c)
+console.log(a != c)
