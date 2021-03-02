d8e5cf61ec7406cf2d842e24aa9e1f8c9d5c9661
hugomacielads <hugomacielcesar@gmail.com>
2021-03-02 01:20:05 +0000

    (01/03) Busca binária

--- 02-busca-sequencial-obj.mjs
+++ 02-busca-sequencial-obj.mjs
@@ -1,6 +1,6 @@
-function buscaSequencial (vetor, valorBusca, fnComp) {
+function buscaSequencial (vetor, fnComp) {
      for(let i = 0; i < vetor.length; i++) {
-         if (fnComp(vetor[i], valorBusca)) return i // Retorna a posição onde foi encontrado 
+         if (fnComp(vetor[i])) return i // Retorna a posição onde foi encontrado 
      }
      return -1
 }
@@ -9,11 +9,28 @@ function buscaSequencial (vetor, valorBusca, fnComp) {
 function comparaNome(objNome, valorBusca) {
     return objNome.first_name === valorBusca
 }
+
+OBS: fnComp = comparaNome (verificar que uma função é passada como parâmetro de uma função)
 */
 
-const comparaNome = (objNome, valorBusca) => obj.Nome.first_name === valorBusca
+// const comparaNome = (objNome, valorBusca) => objNome.first_name === valorBusca
+// const comparaGrupo = (objNome, valorBusca) => objNome.group_name === valorBusca
+
+// console.log(buscaSequencial(objNomes, 'HUGO', (obj, busca) => obj.first_name === busca))    // comparaNome
+// console.log(buscaSequencial(objNomes, 'TULIO', (obj, busca) => obj.group_name === busca))   // comparaGrupo
 
 import { objNomes } from './includes/vetor-obj-nomes.mjs'
 
-console.log(buscaSequencial(objNomes, 'HUGO', comparaNome))
 
+
+console.log(buscaSequencial(objNomes, obj => obj.first_name === 'HUGO'))
+console.log(buscaSequencial(objNomes, obj => obj.group_name === 'TULIO'))
+
+// Primeiro nome da lista que tem mais de 1000 pessoas registradas com ele
+let pos = buscaSequencial(objNomes, obj => obj.frequency_total > 1000)
+
+console.log(objNomes[pos])
+
+// Primeiro nome masculino da lista com mais de 10000 registros
+pos = buscaSequencial(objNomes, obj => obj.frequency_total > 10000 && obj.classification === 'M')
+console.log(objNomes[pos])
